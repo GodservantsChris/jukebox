fdb62cc748a91f699b4f066ce7f85dd5fe167330
Chris J. Miller <godservantschris@gmail.com>
2025-11-12 13:18:32 -0500

    Stub out sampling.py.

---
+++ scripts/Sampling/sampling.py
@@ -0,0 +1,12 @@
+from jukebox import sample
+
+def sample_lyrics_1b():
+    emsgContext = f"sampling.sample_lyrics_1b"
+    emsgOperation = f""
+    try:        
+        print(f"Sampling...")
+    except:
+        print(f"Exception in " + emsgContext + f" while " + emsgOperation)
+    finally:
+        print(f"Done.")    
+
