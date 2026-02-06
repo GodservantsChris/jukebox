6b0015db33d6a5a8e04f47f285f29b8a9fcd9f0e
GodservantsChris <GodservantsChris@gmail.com>
2026-02-06 14:59:18 -0500

    conditioners.py - LabelConditioner class - forward() - Revert all changes during calling y_emb()
    prior.py - SimplePrior class - get_cond() - Revert all changes during calling y_emb()

--- jukebox/prior/conditioners.py
+++ jukebox/prior/conditioners.py
@@ -274,9 +274,7 @@ class LabelConditioner(nn.Module):
         try:        
             assert len(y.shape) == 2, f"Expected shape with 2 dims, got {y.shape}"
             assert y.shape[-1] == 4 + self.max_bow_genre_size, f"Expected shape (N,{4 + self.max_bow_genre_size}), got {y.shape}"
-            dtype_expected = t.int64
-            if t.cuda.is_available() : dtype_expected = t.cuda.LongTensor
-            assert (y.dtype == dtype_expected), f"Expected dtype {dtype_expected}, got {y.dtype}"
+            assert isinstance(y, t.cuda.LongTensor), f"Expected dtype {t.cuda.LongTensor}, got {y.dtype}"
             N = y.shape[0]
             total_length, offset, length, artist, genre = y[:,0:1], y[:,1:2], y[:,2:3], y[:,3:4], y[:,4:]
 
