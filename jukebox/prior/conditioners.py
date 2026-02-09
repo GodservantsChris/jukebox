d365a02c5932380071d5da50557129a87c806c06
GodservantsChris <GodservantsChris@gmail.com>
2026-02-09 13:47:38 -0500

    conditioner.py - LabelConditioner.forward() - Update assert for y.dtype

--- jukebox/prior/conditioners.py
+++ jukebox/prior/conditioners.py
@@ -274,10 +274,8 @@ class LabelConditioner(nn.Module):
         try: 
             assert len(y.shape) == 2, f"Expected shape with 2 dims, got {y.shape}"
             assert y.shape[-1] == 4 + self.max_bow_genre_size, f"Expected shape (N,{4 + self.max_bow_genre_size}), got {y.shape}"
-            type_expected = t.LongTensor
-            if t.cuda.is_available() : type_expected = t.cuda.LongTensor
-            emsgOperation = f"asserting to validate y"       
-            assert isinstance(y, type_expected), f"Expected type for y: {type_expected}, got: " + str(y.type())
+            dtype_expected = t.long
+            assert (y.dtype == dtype_expected), f"Expected dtype {dtype_expected}, got {y.dtype}"
             raise NameError(emsgContext + f" while " + emsgOperation + f"; Stop execution here.")
             N = y.shape[0]
             total_length, offset, length, artist, genre = y[:,0:1], y[:,1:2], y[:,2:3], y[:,3:4], y[:,4:]
